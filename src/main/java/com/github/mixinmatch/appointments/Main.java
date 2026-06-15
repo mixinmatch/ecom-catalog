c0e0aeb2f980516f3ef7b3da197b63614a1eb616
wayne <chevreuilqwerty@gmail.com>
2026-06-15 15:48:03 -0400

    Fix

--- src/main/java/com/github/mixinmatch/appointments/Main.java
+++ src/main/java/com/github/mixinmatch/appointments/Main.java
@@ -1,18 +1,12 @@
 package com.github.mixinmatch.appointments;
 
-import com.azure.spring.messaging.implementation.annotation.EnableAzureMessaging;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-
-import java.util.Collections;
 
 @SpringBootApplication
-@EnableAzureMessaging
 public class Main {
 
-    static void main() {
+    public static void main(String[] args) {
         SpringApplication.run(Main.class);
     }
 }
--- src/main/java/com/github/mixinmatch/appointments/sub/ServiceBus.java
+++ src/main/java/com/github/mixinmatch/appointments/sub/ServiceBus.java
@@ -3,6 +3,9 @@ package com.github.mixinmatch.appointments.sub;
 import com.azure.spring.messaging.servicebus.implementation.core.annotation.ServiceBusListener;
 import com.github.mixinmatch.appointments.databases.CatalogRepo;
 import com.github.mixinmatch.appointments.models.Message;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.QueueTrigger;
+import com.microsoft.azure.functions.annotation.ServiceBusQueueTrigger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import tools.jackson.databind.JsonNode;
@@ -18,8 +21,15 @@ public class ServiceBus {
         this.catalogRepo = repo;
     }
 
-    @ServiceBusListener(destination = "catalog-queue")
-    public void markAsLiquidationItem(String message) {
+    @FunctionName("markAsLiquidationItem")
+    public void markAsLiquidationItem(
+            @ServiceBusQueueTrigger(
+                    name = "queue",
+                    queueName = "catalog-queue",
+                    connection = "ServiceBusConnectionString"
+            )
+            String message
+    ) {
         System.out.println("Message.java: " + message);
         JsonNode msg2 = om.readTree(message);
         JsonNode node = msg2.path("content").path("isLiquidated");
